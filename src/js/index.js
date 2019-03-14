3ac3db69b11c90013e17d5af6502b14e26669f45
Rafael Perozin <RafaelP@sbsnet.local>
2019-03-14 13:14:35 +0000

    start to dev controlSearch

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,33 @@
-import axios from 'axios';
+import Search from './models/Search';
 
-// if don't work, use crossorigin as a prefix proxy on the url
-async function getResults(query) {
-    const key = '6148fe8657b39087371ebd296d5d1d37';
-    // use fetch, but axios return directly in JSON
-    try {
-        const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-        const recipes = res.data.recipes;
-        console.log(recipes);
-    } catch (error) {
-        alert(error);
+// * Global state of the app
+// * - Search object
+// * - Current recipe object
+// * - Shopping list objetc
+// * - Liked recipes
+const state = {};
+
+const controlSearch = async () => {
+
+    // 1. get query from view
+    const query = 'pizza'; // !TODO
+
+    // if ter is a query
+    if (query) {
+        // 2. News search object and add to state
+        state.search = new Search(query);
+
+        // 3. Prepare UI for recipes
+
+        // 4. Search for recipes and await the result
+        await state.search.getResults();
+
+        // 5. Render results on UI
+        console.log(state.search.result);
     }
 }
-getResults('tomato pasta');
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+})
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,20 @@
-export default 'I am exported from Search.js';
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    // if don't work, use crossorigin as a prefix proxy on the url
+    async getResults() {
+        const key = '6148fe8657b39087371ebd296d5d1d37';
+        // use fetch, but axios return directly in JSON
+        try {
+            const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+            // console.log(this.result);
+        } catch (error) {
+            alert(error);
+        }
+    }
+}
