39bf65bb5f773615daefa3a46a50da9347c9b485
Rafael Perozin <RafaelP@sbsnet.local>
2019-04-16 14:12:14 +0100

    BUild Recipe View Part 1

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 import {
     elements,
     renderLoader,
@@ -77,7 +78,9 @@ const controlRecipe = async () => {
     // just run if exist some value there
     if (id) {
 
-        // ! Prepare UI for changes
+        // Prepare UI for changes
+        recipeView.clearRecipe();
+        renderLoader(elements.recipe);
 
         // Create new recipe objetc
         state.recipe = new Recipe(id);
@@ -97,7 +100,9 @@ const controlRecipe = async () => {
             state.recipe.calcServings();
 
             // Render recipe
-            console.log(state.recipe);
+            clearLoader();
+            recipeView.renderRecipe(state.recipe);
+
         } catch (err) {
             alert(`Recipe: #1 Error processing [${err}]`);
         }
--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -34,6 +34,8 @@ export default class Recipe {
         
         const unitsLong = ['tablespoons', 'tablespoon', 'ounces', 'ouce', 'teaspoons', 'teaspoon', 'cups', 'pounds'];
         const unitsShort = ['tbsp', 'tbsp', 'oz', 'oz', 'tsp', 'tsp', 'cup', 'pound'];
+        const units = [...unitsShort, 'kg', 'g'];
+
         const nnewIngredients = this.ingredients.map(el => {
             
             // 1) Uniform units
@@ -48,7 +50,7 @@ export default class Recipe {
             // 3) Parse ingredients into count, unit and ingredient
             const arrIng = ingredient.split(' ');
             // indexOf doesn't work because I don't now which unit I'm looking for
-            const unitIndex = arrIng.findIndex(el2 => unitsShort.includes(el2));
+            const unitIndex = arrIng.findIndex(el2 => units.includes(el2));
 
             let objIng;
             if (unitIndex > -1) {
